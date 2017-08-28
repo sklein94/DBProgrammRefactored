125f98ef5e419d965ebcfe9c1f662bc7173156e0
sklein <simon.klein.sz.1@gmail.com>
2017-08-28 08:50:51 +0200

    Included Jar, Created Main Class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    private static final String HOSTNAME = "egmontix";
+    private static final String PORT = "1521";
+    private static final String SID = "dev3";
+    private static final String USERNAME = "sklein";
+    private static final String PASSWORD = "sklein";
+
+    public static void main (String[] args){
+    }
+}
